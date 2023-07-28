85bd88e32932ccc1cd630a0d6a10e292f93f919d
Min Zaw Htay <91814890+MinZawHtay@users.noreply.github.com>
2023-07-28 16:49:30 +0630

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import time
-import autopy
+import pyautogui
+index_y = 0
 cap= cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
+screen_w,screen_h = pyautogui.size()
 while True:
     success, image = cap.read()
     image = cv2.flip(image,1)
@@ -20,8 +22,19 @@ while True:
             for id , landmark in enumerate(landmarks):
                     x = int(landmark.x * frame_w)
                     y = int(landmark.y * frame_h)
-                    print(x,y)
-
+                    if id == 8:
+                         cv2.circle(img=image,center=(x,y),radius=20,color=(255,0,255))
+                         index_x =screen_w / frame_w * x
+                         index_y = screen_h / frame_h * y
+                         pyautogui.moveTo(index_x,index_y)
+                    if id == 4:
+                         cv2.circle(img=image,center=(x,y),radius=20,color=(255,0,255))
+                         thumb_x =screen_w / frame_w * x
+                         thumb_y = screen_h / frame_h * y
+                         print("Outside",abs(index_y - thumb_y))
+                         if abs(index_y - thumb_y) < 20:
+                              pyautogui.click()
+                              pyautogui.sleep(1)
     # print(hands)
     cv2.imshow("Image",image)
     cv2.waitKey(1)
